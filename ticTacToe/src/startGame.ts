1e9d71407923159b31c0b02833dd4674d3f1cd47
chetelev <chetelev@gmail.com>
2019-09-16 16:34:53 +0200

    Completed ticTacToe task

--- ticTacToe/src/startGame.ts
+++ ticTacToe/src/startGame.ts
@@ -1,60 +1,141 @@
+import { transcode } from "buffer";
+import { brotliDecompressSync } from "zlib";
+
 export class Game {
+    // Variable declaration
     private modalToggle: HTMLElement;
     private playX: HTMLElement;
     private playO: HTMLElement;
-    private app: HTMLElement;
-    private target: HTMLElement
     private plays: NodeListOf<HTMLElement>;
     private canPlay: boolean;
+    private playerToken: string;
+    private aiToken: string;
+    private turn: number;
+    private message: HTMLElement;
+    private dark: HTMLElement;
+    private playerMoveGlobal: any;
+    private stopGame: boolean;
 
     constructor() {
+        // DOM Selectors
         this.modalToggle = document.getElementById('dark');
-        this.playX = document.getElementById('playX')
-        this.playO = document.getElementById('playO')
+        this.playX = document.getElementById('playX');
+        this.playO = document.getElementById('playO');
         this.plays = document.querySelectorAll(".cell");
+        this.message = document.getElementById('message');
+        this.dark = document.getElementById('dark');
+        this.turn = 0;
         this.canPlay = false;
+        this.stopGame = false;
         this.startGame();
     }
 
     startGame() {
-        this.canPlay = true;
+        // Modal trigger and choose sign
         this.modalToggle.classList.remove('hidden');
         this.playX.addEventListener('click', () => {
             this.modalToggle.classList.add('hidden');
-            this.playerMove('X');
+            this.playerToken = 'X';
+            this.aiToken = 'O';
+            this.playerMove(this.playerToken);
         });
         this.playO.addEventListener('click', () => {
             this.modalToggle.classList.add('hidden');
-            this.playerMove('O');
+            this.playerToken = 'O';
+            this.aiToken = 'X';
+            this.playerMove(this.playerToken);
         });
     }
 
     playerMove(token: string) {
-        this.app = document.getElementById('app')
-        this.app.addEventListener('click', (e) => {
-            if (this.canPlay) {
-                this.target = e.target as HTMLElement;
-                this.target.innerHTML = token;
-                this.target.classList.remove('playable')
-                this.target.setAttribute('disabled', 'true')
-                this.aiMove('O');
+        // Instance of global this 
+        var self = this;
+        // PlayerMove function
+        this.playerMoveGlobal = function (e: any) {
+            if (self.stopGame === false) {
+                let target = e.target as HTMLElement;
+                target.innerHTML = token;
+                target.classList.remove('playable');
+                target.removeEventListener('click', self.playerMoveGlobal)
+                self.checker('player');
+                setTimeout(() => {
+                    if (self.stopGame === false) {
+                        self.aiMove(self.aiToken);
+                    }
+                }, 300);
+
+                self.turn++
             }
-        })
+        }
+        for (var i = 0; i < this.plays.length; i++) {
+            this.plays[i].addEventListener("click", this.playerMoveGlobal);
+        }
     }
 
     aiMove(token: string) {
-        let randIndex: number = Math.floor(Math.random() * this.plays.length);
-        while (this.plays[randIndex].innerHTML === 'X' || this.plays[randIndex].innerHTML === 'O') {
-            randIndex = Math.floor(Math.random() * this.plays.length);
+        // Generate random number
+        let randomNumber: number = Math.floor(Math.random() * 9);
+        // Algorithm for computer turn
+        while (this.plays[randomNumber].innerHTML === 'X' || this.plays[randomNumber].innerHTML === 'O' && this.turn < 5) {
+            randomNumber = Math.floor(Math.random() * 9);
         }
-        this.plays[randIndex].innerHTML = token;
-        this.plays[randIndex].classList.remove('playable');
-        //this.checker.checker();
-        //this.plays[randIndex].classList.add('class-' + compSign);
+        this.plays[randomNumber].removeEventListener('click', this.playerMoveGlobal)
+        this.plays[randomNumber].innerHTML = token;
+        this.plays[randomNumber].classList.remove('playable');
+        this.checker("comp");
+        console.log(this.turn)
+    }
+    checker(who: string) {
+        if ((who === "player")
+            // Win condition
+            && ((this.plays[0].innerHTML === this.playerToken && this.plays[1].innerHTML === this.playerToken && this.plays[2].innerHTML === this.playerToken)
+                || (this.plays[3].innerHTML === this.playerToken && this.plays[4].innerHTML === this.playerToken && this.plays[5].innerHTML === this.playerToken)
+                || (this.plays[6].innerHTML === this.playerToken && this.plays[7].innerHTML === this.playerToken && this.plays[8].innerHTML === this.playerToken)
+                || (this.plays[0].innerHTML === this.playerToken && this.plays[3].innerHTML === this.playerToken && this.plays[6].innerHTML === this.playerToken)
+                || (this.plays[1].innerHTML === this.playerToken && this.plays[4].innerHTML === this.playerToken && this.plays[7].innerHTML === this.playerToken)
+                || (this.plays[2].innerHTML === this.playerToken && this.plays[5].innerHTML === this.playerToken && this.plays[8].innerHTML === this.playerToken)
+                || (this.plays[0].innerHTML === this.playerToken && this.plays[4].innerHTML === this.playerToken && this.plays[8].innerHTML === this.playerToken)
+                || (this.plays[2].innerHTML === this.playerToken && this.plays[4].innerHTML === this.playerToken && this.plays[6].innerHTML === this.playerToken))
+        ) {
+            this.stopGame = true;
+            setTimeout(() => {
+                this.reset('You win !')
+            }, 1000);
+            // Lose condition
+        } else if ((who === "comp")
+            && ((this.plays[0].innerHTML === this.aiToken && this.plays[1].innerHTML === this.aiToken && this.plays[2].innerHTML === this.aiToken)
+                || (this.plays[3].innerHTML === this.aiToken && this.plays[4].innerHTML === this.aiToken && this.plays[5].innerHTML === this.aiToken)
+                || (this.plays[6].innerHTML === this.aiToken && this.plays[7].innerHTML === this.aiToken && this.plays[8].innerHTML === this.aiToken)
+                || (this.plays[0].innerHTML === this.aiToken && this.plays[3].innerHTML === this.aiToken && this.plays[6].innerHTML === this.aiToken)
+                || (this.plays[1].innerHTML === this.aiToken && this.plays[4].innerHTML === this.aiToken && this.plays[7].innerHTML === this.aiToken)
+                || (this.plays[2].innerHTML === this.aiToken && this.plays[5].innerHTML === this.aiToken && this.plays[8].innerHTML === this.aiToken)
+                || (this.plays[0].innerHTML === this.aiToken && this.plays[4].innerHTML === this.aiToken && this.plays[8].innerHTML === this.aiToken)
+                || (this.plays[2].innerHTML === this.aiToken && this.plays[4].innerHTML === this.aiToken && this.plays[6].innerHTML === this.aiToken))
+        ) {
+            this.stopGame = true;
+            setTimeout(() => {
+                this.reset('You lose !')
+            }, 1000);
+        }
+        // Tie condition
+        else if (this.turn === 6) {
+            this.stopGame = true;
+            setTimeout(() => {
+                this.reset('You tied !')
+            }, 1000);
+        }
     }
 
-    checker(checker: string) {
-
+    reset(msg: string) {
+        // Reset method for modal pop after game has finished
+        this.playX.classList.add("hidden");
+        this.playO.classList.add("hidden");
+        this.modalToggle.classList.remove('hidden')
+        this.message.textContent = msg;
+        this.message.classList.remove("hidden");
+        this.dark.classList.remove("hidden");
+        setTimeout(function () {
+            location.reload()
+        }, 1500);
     }
-
 }
